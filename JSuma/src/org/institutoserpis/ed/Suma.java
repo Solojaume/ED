9d682c195077a5b8eaf441501ecef6aea1ad5c4c
Solojaume <jaumeponsorti@gmail.com>
2018-10-08 10:43:34 +0200

    Añadir jsuma con metodo Bigdecimal y hemos actualizado el archivo de comandos basicos

---
+++ JSuma/src/org/institutoserpis/ed/Suma.java
@@ -0,0 +1,24 @@
+package org.institutoserpis.ed;
+import java.util.*;
+import java.math.BigDecimal;
+
+public class Suma {
+	public static void main(String[] args) {
+		Scanner tcl = new Scanner (System.in);
+		System.out.print("Introduce un número: ");
+	    BigDecimal numero1= new BigDecimal(tcl.nextLine());
+	    System.out.print("Introduce un número: ");
+	    BigDecimal numero2= new BigDecimal(tcl.nextLine());
+		System.out.println("Primer número = "+numero1);
+		System.out.println("Primer número = "+numero2);
+		
+		BigDecimal suma =numero1.add(numero2);
+		BigDecimal resta =numero1.subtract(numero2);
+		BigDecimal producto =numero1.multiply(numero2);
+		BigDecimal dividir =numero1.divide(numero2);
+	    System.out.print("Suma ="+suma);
+	    System.out.print("resta ="+resta);
+	    System.out.print("Producto ="+producto);
+	    System.out.print("Dividir ="+dividir);
+	}
+}

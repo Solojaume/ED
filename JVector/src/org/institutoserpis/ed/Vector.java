ab6075e73ddea13b65b33e48022fe5f84fe62f39
Solojaume <jaumeponsorti@gmail.com>
2018-10-25 12:14:11 +0200

    Actualizado JVector para hacer minimo y test para comprobar

--- JVector/src/org/institutoserpis/ed/Vector.java
+++ JVector/src/org/institutoserpis/ed/Vector.java
@@ -1,15 +1,35 @@
 package org.institutoserpis.ed;
 
 public class Vector {
-
-	public static void main(String[] args) {
+	
+    public static int indexOf(int[] v,int x) {
+    	
+    	int index=0;
+    	for(int item : v)
+    		System.out.println(item);
+        while ( index < v.length && v[index]!=x) {
+        	index++;	
+        }
+        if (index == v.length)
+        	return -1;
+        return index;
+    }
+    public static int min(int[] v) {
+    	int min=v[0];
+    	for (int i = 1; i < v.length; i++) {
+    		if(v[i]< min )
+    			  min=v[i];
+		}
+        return min;
+    }
+    
+    public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] v= new int[] {16,12,15,14, 17};
-		int x=14;
-		int index=indexOf(v,14);
+		
+		int x=12;
+		int index=indexOf(v,x);
 		System.out.println("index="+ index);
+		int min=min(v);
 	}
-    private static int indexOf(int[] v,int x) {
-    	return 3;
-    }
 }

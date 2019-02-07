4e0081ab2f871d3ef5df1eb18ba286ac95b79bda
Solojaume <jaumeponsorti@gmail.com>
2019-02-07 12:27:54 +0100

    ASuma funcional

--- ASuma/app/src/main/java/com/example/asuma/MainActivity.java
+++ ASuma/app/src/main/java/com/example/asuma/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.asuma;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,17 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void sumar(){
+    public void sumar(View view){
         EditText introNumero1 = findViewById(R.id.introNumero1);
         EditText introNumero2 = findViewById(R.id.introNumero2);
         TextView textView= findViewById(R.id.textView);
 
 
         String cadenaN1 = introNumero1.getText().toString();
-        String cadenaN2 = introNumero1.getText().toString();
+        String cadenaN2 = introNumero2.getText().toString();
         Double n1 = Double.parseDouble(cadenaN1);
         Double n2 = Double.parseDouble(cadenaN2);
         double suma = n1+n2;
-        textView.setText(suma);
+        textView.setText(""+suma);
     }
 }

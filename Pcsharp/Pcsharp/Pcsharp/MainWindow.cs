5cc361798098b08788e6eb35eec2ebbb67d56ff0
Solojaume <jaumeponsorti@gmail.com>
2019-05-02 12:18:05 +0200

    añadido pcsharp

---
+++ Pcsharp/Pcsharp/Pcsharp/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        treeView.AppendColumn("Nombre", new CellRendererText(),"text",0);
+        treeView.AppendColumn("Telefono", new CellRendererText(), "text", 1);
+        ListStore listStore = new ListStore(typeof(String), typeof(String));
+            treeView.Model =listStore;
+        listStore.AppendValues("Rosa", "888");
+
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
